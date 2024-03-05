97fce1febd94956dabbee64fa819359e8636282b
tatojajo <tatojajanidze@gmail.com>
2024-03-05 13:23:47 +0100

    last changes

--- quiz_project/quiz.py
+++ quiz_project/quiz.py
@@ -1,4 +1,21 @@
 
+
+def run_quiz(questions):
+    score  = 0
+    for question in questions:
+        print(question['prompt'])
+        for option in question['options']:
+            print(option)
+        answer = input('Enter your answer (A, B, C): ')
+        if answer.upper() == question['answer']:
+            print('Correct \n')
+            score+=1
+        else:
+            print('Wrong!! The correct answer is', question["answer"], '\n')
+            
+    print(f'You got {score} out of {len(questions)} questions correct.')
+            
+         
         
 
 questions = [
@@ -32,23 +49,7 @@ questions = [
         "options": ["A. 2007", "B. 2009", "C. 2010"],
         "answer": "A"
     }
-]
+]   
 
-def run_quiz():
-    score  = 0
-    for question in questions:
-        print(question['prompt'])
-        for option in question['options']:
-            print(option)
-        answer = input('Enter your answer (A, B, C): ')
-        if answer.upper() == question['answer']:
-            print('Correct \n')
-            score+=1
-        else:
-            print('Wrong!! The correct answer is', question["answer"], '\n')
-            
-    print(f'You got {score} out of {len(questions)} questions correct.')
-            
-            
 
-run_quiz()
+run_quiz(questions)
